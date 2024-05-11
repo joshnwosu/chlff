de3249ba888b28d2bf24e560725e57ba86778273
joshnwosu <joshuanwosu078@gmail.com>
2024-05-11 19:39:17 +0100

    update

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -2,67 +2,118 @@ import React, { useState, useEffect } from 'react';
 import './Game.css';
 
 export default function Game() {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [direction, setDirection] = useState('right');
+  const [position, setPosition] = useState({ y: 0 });
+  // const [direction, setDirection] = useState('right');
+
+  // useEffect(() => {
+  //   const handleKeyPress = (e: any) => {
+  //     switch (e.key) {
+  //       case 'ArrowUp':
+  //         setDirection('up');
+  //         break;
+  //       case 'ArrowDown':
+  //         setDirection('down');
+  //         break;
+  //       case 'ArrowLeft':
+  //         setDirection('left');
+  //         break;
+  //       case 'ArrowRight':
+  //         setDirection('right');
+  //         break;
+  //       default:
+  //         break;
+  //     }
+  //   };
+
+  //   window.addEventListener('keydown', handleKeyPress);
+
+  //   return () => {
+  //     window.removeEventListener('keydown', handleKeyPress);
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     moveCar();
+  //   }, 100);
+
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // }, []);
+
+  // const moveCar = () => {
+  //   switch (direction) {
+  //     case 'up':
+  //       setPosition((prevPos) => ({ ...prevPos, y: prevPos.y - 1 }));
+  //       break;
+  //     case 'down':
+  //       setPosition((prevPos) => ({ ...prevPos, y: prevPos.y + 1 }));
+  //       break;
+  //     case 'left':
+  //       setPosition((prevPos) => ({ ...prevPos, x: prevPos.x - 1 }));
+  //       break;
+  //     case 'right':
+  //       setPosition((prevPos) => ({ ...prevPos, x: prevPos.x + 1 }));
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // };
+
+  // const [position, setPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    const handleKeyPress = (e: any) => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      const parentHeight = 200; // Height of the parent container
+      const childHeight = 50; // Height of the child div
+
       switch (e.key) {
         case 'ArrowUp':
-          setDirection('up');
+          setPosition((prevPosition) => ({
+            ...prevPosition,
+            y: Math.max(0, prevPosition.y - 200),
+          }));
           break;
         case 'ArrowDown':
-          setDirection('down');
-          break;
-        case 'ArrowLeft':
-          setDirection('left');
-          break;
-        case 'ArrowRight':
-          setDirection('right');
+          setPosition((prevPosition) => ({
+            ...prevPosition,
+            y: Math.min(parentHeight - childHeight, prevPosition.y + 200),
+          }));
           break;
         default:
           break;
       }
     };
 
-    window.addEventListener('keydown', handleKeyPress);
+    window.addEventListener('keydown', handleKeyDown);
 
     return () => {
-      window.removeEventListener('keydown', handleKeyPress);
+      window.removeEventListener('keydown', handleKeyDown);
     };
-  }, []);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      moveCar();
-    }, 100);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  const moveCar = () => {
-    switch (direction) {
-      case 'up':
-        setPosition((prevPos) => ({ ...prevPos, y: prevPos.y - 1 }));
-        break;
-      case 'down':
-        setPosition((prevPos) => ({ ...prevPos, y: prevPos.y + 1 }));
-        break;
-      case 'left':
-        setPosition((prevPos) => ({ ...prevPos, x: prevPos.x - 1 }));
-        break;
-      case 'right':
-        setPosition((prevPos) => ({ ...prevPos, x: prevPos.x + 1 }));
-        break;
-      default:
-        break;
-    }
-  };
+  }, []); // Run effect only once on mount
   return (
-    <div className='game-container'>
-      <div className='car'></div>
-    </div>
+    <>
+      <div className='game-container'>
+        <div
+          className='car'
+          style={{
+            position: 'absolute',
+            top: position.y,
+            left: '50px',
+            width: '100px',
+            height: '50px',
+            backgroundColor: 'blue',
+            transition: 'top 0.2s ease-in-out',
+          }}
+        ></div>
+      </div>
+
+      <div className='road'>
+        <div className='lane'></div>
+        <div className='centerLine'></div>
+        <div className='lane'></div>
+      </div>
+    </>
   );
 }
