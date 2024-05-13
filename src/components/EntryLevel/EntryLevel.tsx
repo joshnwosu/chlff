38cec934ecf530ed88c920e4e21e0593072883c5
joshnwosu <joshuanwosu078@gmail.com>
2024-05-13 23:38:14 +0100

    update

--- src/components/EntryLevel/EntryLevel.tsx
+++ src/components/EntryLevel/EntryLevel.tsx
@@ -3,19 +3,27 @@ import './EntryLevel.css';
 
 const EntryLevel: React.FC = () => {
   return (
-    <div className='layout'>
-      <div className='screen'>
-        <div className='animation'>
-          <div className='question'></div>
+    <div className='container'>
+      <h1>Year 2 Assessment Question</h1>
+
+      <div className='layout'>
+        {/* <div className='flex'> */}
+        <div className='screen'>
+          <div className='animation'>
+            <div className='question'>
+              <p>Hello world?</p>
+            </div>
+          </div>
+          <div className='options'>
+            <CustomButton>Option 1</CustomButton>
+            <CustomButton>Option 2</CustomButton>
+            <CustomButton>Option 3</CustomButton>
+            <CustomButton>Option 4</CustomButton>
+          </div>
         </div>
-        <div className='options'>
-          <CustomButton>Option 1</CustomButton>
-          <CustomButton>Option 2</CustomButton>
-          <CustomButton>Option 3</CustomButton>
-          <CustomButton>Option 4</CustomButton>
-        </div>
+        {/* </div> */}
+        <div className='screen-info'></div>
       </div>
-      <div className='screen-info'></div>
     </div>
   );
 };
