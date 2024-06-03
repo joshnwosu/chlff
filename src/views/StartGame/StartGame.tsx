e089c853c380463ff0619a7fbedcf7ea7e20c254
joshnwosu <joshuanwosu078@gmail.com>
2024-06-03 21:35:52 +0100

    update

---
+++ src/views/StartGame/StartGame.tsx
@@ -0,0 +1,21 @@
+import './StartGame.css';
+import { useAppDispatch } from '../../app/hooks';
+import { toggleStartGame } from '../../features/control/controlSlice';
+import CustomButton from '../../components/Shared/CustomButton/CsutomButton';
+
+const StartGame: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const handleStart = async () => {
+    dispatch(toggleStartGame(true));
+  };
+  return (
+    <div className='start_game'>
+      <h3>Ready for your Assessment Test?</h3>
+      <div>
+        <CustomButton onClick={handleStart}>START NOW</CustomButton>
+      </div>
+    </div>
+  );
+};
+
+export default StartGame;
