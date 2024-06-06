00f97913dcecd5ac132301786eabcbfc9491d0b1
joshnwosu <joshuanwosu078@gmail.com>
2024-06-06 16:37:52 +0100

    update

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Outlet,
+} from 'react-router-dom';
 import { ProtectedRoute, PublicRoute } from './AuthRoutes';
 import { useAppSelector } from '../app/hooks';
 import Root from '../components/Root/Root';
@@ -6,6 +11,7 @@ import Login from '../views/Auth/Login/Login';
 import StartGame from '../views/StartGame/StartGame';
 import Assessment from '../views/Assessment/Assessment';
 import ActionCenter from '../views/ActionCenter/ActionCenter';
+import SelectLevel from '../views/SelectLevel/SelectLevel';
 
 export default function AppRoutes() {
   const { isAuthenticated } = useAppSelector((state) => state.auth);
@@ -23,7 +29,11 @@ export default function AppRoutes() {
         >
           <Route index element={<StartGame />} />
           <Route path='assessment' element={<Assessment />} />
-          <Route path='action-center' element={<ActionCenter />} />
+
+          <Route path='action-center' element={<Outlet />}>
+            <Route index element={<ActionCenter />} />
+            <Route path=':id/select-level' element={<SelectLevel />} />
+          </Route>
         </Route>
 
         <Route
--- src/views/ActionCenter/ActionCenter.tsx
+++ src/views/ActionCenter/ActionCenter.tsx
@@ -1,27 +1,32 @@
 import classes from './ActionCenter.module.css';
 import LeaderBoard from '../../components/LeaderBoard/LeaderBoard';
 import PageWrapper from '../../components/Shared/PageWrapper/PageWrapper';
+import { Link } from 'react-router-dom';
 
 const options = [
   {
     name: 'Times Table',
     color: 'rgba(70, 107, 163, 0.9)',
     img: '/assets/car_race1.jpeg',
+    link: 'times-table',
   },
   {
     name: 'Phonics',
     color: 'rgba(198, 81, 149, 0.9)',
     img: '/assets/phonics_image1.jpeg',
+    link: 'phonics',
   },
   {
     name: 'Punctuations',
     color: 'rgba(17, 169, 182, 0.9)',
     img: '/assets/punctuation_image_for_children1.jpeg',
+    link: 'punctuations',
   },
   {
     name: 'Spellings',
     color: 'rgba(245, 178, 22, 0.9)',
     img: '/assets/spelling_image1.jpeg',
+    link: 'spellings',
   },
 ];
 
@@ -30,31 +35,33 @@ const ActionCenter: React.FC = () => {
     <PageWrapper>
       <div className={classes.actionCenter}>
         <div className={classes.actionCenterLeft}>
-          <LeaderBoard />
+          {false && <LeaderBoard />}
         </div>
         <div className={classes.actionCenterMiddle}>
           <div className={classes.actionCenterGameCardContainer}>
             {options.map((item, index) => (
-              <div
-                className={classes.actionCenterGameCard}
-                key={index.toString()}
-              >
-                {item.img && (
-                  <img
-                    src={item.img}
-                    className={classes.actionCenterGameCardImage}
-                  />
-                )}
+              <Link to={`${item.link}/select-level`} key={index.toString()}>
                 <div
-                  className={classes.actionCenterGameCardOverlay}
-                  style={{
-                    backgroundImage: `linear-gradient(45deg, ${item.color} 35%, ${item.color} 35%, transparent)`,
-                  }}
-                />
-                <div className={classes.actionCenterGameCardContent}>
-                  <p>{item.name}</p>
+                  className={classes.actionCenterGameCard}
+                  // key={index.toString()}
+                >
+                  {item.img && (
+                    <img
+                      src={item.img}
+                      className={classes.actionCenterGameCardImage}
+                    />
+                  )}
+                  <div
+                    className={classes.actionCenterGameCardOverlay}
+                    style={{
+                      backgroundImage: `linear-gradient(45deg, ${item.color} 35%, ${item.color} 35%, transparent)`,
+                    }}
+                  />
+                  <div className={classes.actionCenterGameCardContent}>
+                    <p>{item.name}</p>
+                  </div>
                 </div>
-              </div>
+              </Link>
             ))}
           </div>
         </div>
