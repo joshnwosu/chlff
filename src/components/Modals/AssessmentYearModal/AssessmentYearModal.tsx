611bc4623bf8a59267b69180ba927a992a6cd95c
joshnwosu <joshuanwosu078@gmail.com>
2024-06-25 22:33:12 +0100

    update

---
+++ src/components/Modals/AssessmentYearModal/AssessmentYearModal.tsx
@@ -0,0 +1,3 @@
+export default function AssessmentYearModal() {
+  return <div>SelectAssessment</div>;
+}
--- src/views/ActionCenter/ActionCenter.tsx
+++ src/views/ActionCenter/ActionCenter.tsx
@@ -5,25 +5,25 @@ import { Link } from 'react-router-dom';
 
 const options = [
   {
-    name: 'Times Table (x)',
+    name: 'MULTIPLICATION',
     color: 'rgba(70, 107, 163, 0.9)',
     img: '/assets/car_race1.jpeg',
     link: 'times-table',
   },
   {
-    name: 'Addition (+)',
+    name: 'ADDITION',
     color: 'rgba(198, 81, 149, 0.9)',
     img: '/assets/phonics_image1.jpeg',
     link: 'addition',
   },
   {
-    name: 'Subtraction (-)',
+    name: 'SUBTRACTION',
     color: 'rgba(17, 169, 182, 0.9)',
     img: '/assets/punctuation_image_for_children1.jpeg',
     link: 'subtraction',
   },
   {
-    name: 'Division (÷)',
+    name: 'DIVISION',
     color: 'rgba(245, 178, 22, 0.9)',
     img: '/assets/spelling_image1.jpeg',
     link: 'division',
