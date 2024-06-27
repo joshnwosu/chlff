777e8bc0925ab4d7ab28c1a0de300ab98ac7d0df
joshnwosu <joshuanwosu078@gmail.com>
2024-06-27 03:40:14 +0100

    update

--- src/components/Modals/AssessmentYearModal/AssessmentYearModal.tsx
+++ src/components/Modals/AssessmentYearModal/AssessmentYearModal.tsx
@@ -10,14 +10,84 @@ export default function AssessmentYearModal() {
   const handleClose = () => {
     dispatch(toggleAssessmentYearModal(!assessmentYearModal));
   };
+
+  const list = [
+    {
+      name: 'Year 1',
+      tags: ['multiplication', 'addition', 'substration'],
+      color: '#EF97AF',
+      image: '/assets/Dictionary-pana.png',
+    },
+    {
+      name: 'Year 2',
+      tags: ['multiplication', 'addition', 'substration', 'division'],
+      color: '#D395E8',
+      image: '/assets/Dictionary-bro.png',
+    },
+    {
+      name: 'Year 3',
+      tags: ['multiplication', 'addition', 'substration'],
+      color: '#61BF88',
+      image: '/assets/Audiobook-bro.png',
+    },
+    {
+      name: 'Year 4',
+      tags: ['multiplication', 'addition', 'substration', 'division'],
+      color: '#31B2EE',
+      image: '/assets/kids wearing masks at school-bro.png',
+    },
+    {
+      name: 'Year 5',
+      tags: ['multiplication', 'addition', 'substration'],
+      color: '#EEBC51',
+      image: '/assets/Raising hand-pana.png',
+    },
+    {
+      name: 'Year 6',
+      tags: ['multiplication', 'addition', 'substration', 'division'],
+      color: '#EA7EB4',
+      image: '/assets/Mathematics-bro.png',
+    },
+  ];
+
   return (
     <Overlay opened={assessmentYearModal} close={handleClose}>
       <div className={classes.container}>
         <div className={classes.selectContainer}>
           <div className={classes.selectHeader}>
-            <p>Pick Your Assessment Year!</p>
+            <p className={classes.selectHeaderTitle}>
+              Pick Your Assessment Year!
+            </p>
           </div>
-          <div className={classes.selectContent}></div>
+          <div className={classes.selectContent}>
+            <div className={classes.selectScroll}>
+              {list.map((item, index) => (
+                <div
+                  key={index.toString()}
+                  className={classes.card}
+                  style={{ backgroundColor: item.color || '#ffffff' }}
+                >
+                  <div className={classes.cardContent}>
+                    <div className={classes.imageContainer}>
+                      <img
+                        src={item.image || '/assets/Dictionary-pana.png'}
+                        style={{ width: 120, zIndex: 1 }}
+                      />
+                      <div className={classes.transparentBG} />
+                    </div>
+                    <p className={classes.cardTitle}>{item.name}</p>
+                    <p className={classes.cardDescription}>
+                      Track your progress through each level and see how you
+                      improve over time. Earn badges and rewards for your
+                      achievements!
+                    </p>
+                  </div>
+
+                  <button className={classes.button}>Start Now</button>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </Overlay>
