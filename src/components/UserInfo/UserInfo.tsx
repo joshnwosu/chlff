2ffc45272768230b31cb23ae57afac271377f4b5
joshnwosu <joshuanwosu078@gmail.com>
2024-07-01 08:48:04 +0100

    user object

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,5 +1,12 @@
 import classes from './UserInfo.module.css';
 
+const user = {
+  user: 'Daniel',
+  grade: 'Year 2',
+  school_name: 'Dell',
+  level: 5,
+};
+
 export default function UserInfo() {
   return <div className={classes.container}>UserInfo</div>;
 }
