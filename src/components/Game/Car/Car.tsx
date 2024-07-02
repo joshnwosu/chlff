63c891a7ed33745ad3cd8405210df9f5ff8d4ac7
joshnwosu <joshuanwosu078@gmail.com>
2024-07-02 03:52:58 +0100

    update

---
+++ src/components/Game/Car/Car.tsx
@@ -0,0 +1,28 @@
+import classes from './Car.module.css';
+import PageWrapper from '../../Shared/PageWrapper/PageWrapper';
+import LeaderBoard from '../../LeaderBoard/LeaderBoard';
+
+export default function Car() {
+  return (
+    <PageWrapper>
+      <div className={classes.gameWrapper}>
+        <div className={classes.title}>
+          <h1>Multiplication</h1>
+        </div>
+
+        <div className={classes.gameCenter}>
+          <div className={classes.gameCenterLeft}>
+            <LeaderBoard />
+          </div>
+          <div className={classes.gameCenterMiddle}>
+            <div className={classes.carContainer}></div>
+            <div className={classes.question}>
+              <h1>5 x 4 = ?</h1>
+            </div>
+          </div>
+          <div className={classes.gameCenterRight}></div>
+        </div>
+      </div>
+    </PageWrapper>
+  );
+}
