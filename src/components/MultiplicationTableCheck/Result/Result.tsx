e1cd48ad39073810fa6cc17d7ba81614395a22dc
joshua <joshuanwosu078@gmail.com>
2024-07-26 13:05:14 +0100

    update: mtc result

--- src/components/MultiplicationTableCheck/Result/Result.tsx
+++ src/components/MultiplicationTableCheck/Result/Result.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import classes from './Result.module.css';
+import CustomButton from '../../Shared/CustomButton/CsutomButton';
 
 interface ResultProps {
   questions: {
@@ -9,6 +11,32 @@ interface ResultProps {
   answers: number[];
   onTryAgain: () => void;
 }
+
+const variants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
+
+const variants2 = {
+  open: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 },
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 },
+  },
+};
 
 const Result: React.FC<ResultProps> = ({ questions, answers, onTryAgain }) => {
   const score = questions.reduce((total, question, index) => {
@@ -20,18 +48,30 @@ const Result: React.FC<ResultProps> = ({ questions, answers, onTryAgain }) => {
       <h2>
         Your Score: {score} / {questions.length}
       </h2>
-      <ul>
+      <motion.ul variants={variants2} className={classes.list_container}>
         {questions.map((question, index) => (
-          <li key={index}>
-            {question.num1} x {question.num2} = {question.num1 * question.num2}(
-            {answers[index] === question.num1 * question.num2
-              ? 'Correct'
-              : 'Wrong'}
-            )
-          </li>
+          <motion.li
+            variants={variants}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+            key={index}
+            className={classes.list}
+          >
+            <p>
+              {`${index + 1}.) ${question.num1} x ${question.num2} = `}
+              {question.num1 * question.num2}
+            </p>
+            <p>
+              (
+              {answers[index] === question.num1 * question.num2
+                ? 'Correct'
+                : 'Wrong'}
+              )
+            </p>
+          </motion.li>
         ))}
-      </ul>
-      <button onClick={onTryAgain}>Try Again</button>
+      </motion.ul>
+      <CustomButton onClick={onTryAgain}>Try Again</CustomButton>
     </div>
   );
 };
