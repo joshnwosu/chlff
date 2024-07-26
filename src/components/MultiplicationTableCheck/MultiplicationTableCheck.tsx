54743ee8312e506c34623bfa1ca3959493d4db3c
joshua <joshuanwosu078@gmail.com>
2024-07-26 16:15:57 +0100

    update: mtc

--- src/components/MultiplicationTableCheck/MultiplicationTableCheck.tsx
+++ src/components/MultiplicationTableCheck/MultiplicationTableCheck.tsx
@@ -38,57 +38,6 @@ const MultiplicationTableCheck: React.FC = () => {
               Let's Go
             </CustomButton>
           </div>
-
-          <p className={classes.text}>
-            Change your input method using the Settings menu
-          </p>
-        </div>
-      </div>
-    );
-  };
-
-  const SecondContent = () => {
-    return (
-      <div>
-        <div className={classes.mtc_wrapper}>
-          <div className={classes.title}>
-            <h1>Multitplication Table Check</h1>
-          </div>
-
-          <div className={classes.content}>
-            <h1>Practice Questions</h1>
-
-            <p className={classes.text}>
-              Learn how the test works with these <strong>3 Practice</strong>{' '}
-              quetsions, then take the full 25 questions test.
-            </p>
-
-            <p className={classes.text}>
-              You will have <strong>6 seconds to answer</strong> each of the
-              question. There is a short pause between quetsions to allow you to
-              prepare for the next one.
-            </p>
-
-            <p className={classes.text}>
-              You can use the on-screen keypad or your keyboard.
-            </p>
-
-            <p className={classes.text}>
-              You don't need to press enter as your answer will be taken when
-              the time runs out.
-            </p>
-
-            <div className={classes.btnWrap}>
-              <CustomButton onClick={() => setActiveIndex(2)}>
-                Start Practice
-              </CustomButton>
-            </div>
-          </div>
-        </div>
-        <div className={classes.btnWrapFlex}>
-          <CustomButton onClick={() => setActiveIndex(0)}>Go back</CustomButton>
-
-          <CustomButton onClick={() => setActiveIndex(1)}>Skip</CustomButton>
         </div>
       </div>
     );
@@ -97,11 +46,9 @@ const MultiplicationTableCheck: React.FC = () => {
   return (
     <PageWrapper>
       <div className={classes.container}>
-        {false && (
-          <>{activeIndex == 0 ? <FirstContent /> : <SecondContent />}</>
-        )}
+        {true && <>{activeIndex == 0 ? <FirstContent /> : <RenderMtc />}</>}
 
-        <RenderMtc />
+        {/* <RenderMtc /> */}
       </div>
     </PageWrapper>
   );
