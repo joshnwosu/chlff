f219a7a3b9ae046e0890019da2cb13460b7d491f
joshua <joshuanwosu078@gmail.com>
2024-08-10 00:47:41 +0100

    all

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -8,7 +8,6 @@ import StartGame from '../views/StartGame/StartGame';
 import Assessment from '../views/Assessment/Assessment';
 import ActionCenter from '../views/ActionCenter/ActionCenter';
 import ShowRoom from '../views/ShowRoom/ShowRoom';
-import Car from '../components/Game/Car/Car';
 import MultiplicationTableCheck from '../components/MultiplicationTableCheck/MultiplicationTableCheck';
 import CarUpdate from '../components/Game/Car/CarUpdate';
 
@@ -29,7 +28,7 @@ export default function AppRoutes() {
           <Route index element={<StartGame />} />
           <Route path='assessment' element={<Assessment />} />
           <Route path='show-room' element={<ShowRoom />} />
-          <Route path='game' element={false ? <Car /> : <CarUpdate />} />
+          <Route path='game' element={<CarUpdate />} />
 
           <Route path='action-center' element={<ActionCenter />} />
           <Route
