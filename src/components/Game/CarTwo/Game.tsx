308c4304ade08263b21e8c27582ffbb23168cb8e
bundtractor <gitract@bundul.io>
2024-08-10 01:41:04 +0100

    improved puzzle game functionality and ui

---
+++ src/components/Game/CarTwo/Game.tsx
@@ -0,0 +1,336 @@
+// import React, { useEffect, useRef, useState } from 'react';
+// import classes from './Car.module.css';
+// import LeaderBoard from '../../LeaderBoard/LeaderBoard';
+// import PlayerStat from '../../UserInfo/PlayerStat';
+// import questionsData from '../../../data/questions/questions.json';
+// import useSound from '../../../utils/useSound';
+// import Overlay from '../../Shared/Overlay/Overlay';
+// import GamePopupModal from '../../Modals/GamePopupModal/GamePopupModal';
+// import ButtonIcon from '../../Shared/CustomButton/ButtonIcon';
+// import StarRating from '../../Shared/StarRating/StarRating';
+
+// interface Question {
+//   question: string;
+//   answer: number;
+// }
+
+// // Define the type for the structure of allQuestions
+// interface AllQuestions {
+//   [key: string]: {
+//     [key: string]: Question[];
+//   };
+// }
+// interface Answer {
+//   id: number;
+//   text: number;
+//   position: number;
+//   left: number;
+  
+// }
+
+// const Car: React.FC = () => {
+//   const [position, setPosition] = useState<'up' | 'down'>('down');
+//   const [move, setMove] = useState<number>(200);
+//   const [questions, setQuestions] = useState<Question[]>([]);
+//   const [currentQuestion, setCurrentQuestion] = useState<Question | null>(null);
+//   const [answers, setAnswers] = useState<Answer[]>([]);
+
+//   const [score, setScore] = useState<number>(0);
+//   const [correctAnswers, setCorrectAnswers] = useState<number>(0);
+//   const [wrongAnswers, setWrongAnswers] = useState<number>(0);
+
+//   const [isPaused, setIsPaused] = useState<boolean>(false);
+//   const [currentYear, setCurrentYear] = useState<string>('Year 1');
+//   const [currentLevel, setCurrentLevel] = useState<string>('Level 1');
+
+//   const [showModal, setShowModal] = useState(false);
+
+//   const [status, setStatus] = useState<string>('');
+
+//   const movingDivRef = useRef<HTMLDivElement>(null);
+//   const roadRef = useRef<HTMLDivElement>(null);
+
+//   const allQuestions: AllQuestions = questionsData;
+
+
+//   useEffect(() => {
+//     const selectedQuestions = allQuestions[currentYear][currentLevel];
+//     setQuestions(selectedQuestions);
+//     setCurrentQuestion(selectedQuestions[0]);
+//   }, [currentYear, currentLevel]);
+
+//   useEffect(() => {
+//     if (!currentQuestion) return;
+
+//     const randomPositions = [20, 200];
+//     const correctPosition =
+//       randomPositions[Math.floor(Math.random() * randomPositions.length)];
+//     const wrongPosition = randomPositions.find(
+//       (pos) => pos !== correctPosition
+//     )!;
+
+//     const roadWidth = roadRef.current?.clientWidth || 0;
+
+//     const newAnswers: Answer[] = [
+//       {
+//         id: 1,
+//         text: currentQuestion.answer,
+//         position: correctPosition,
+//         left: roadWidth,
+//       },
+//       {
+//         id: 2,
+//         text: currentQuestion.answer + Math.floor(Math.random() * 10) + 1,
+//         position: wrongPosition,
+//         left: roadWidth,
+//       },
+//     ];
+
+//     setAnswers(newAnswers);
+//   }, [currentQuestion]);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       if (!isPaused) {
+//         setAnswers(
+//           (prevAnswers) =>
+//             prevAnswers.map((answer) => ({ ...answer, left: answer.left - 20 })) // Increased speed by changing 5 to 10
+//         );
+//       }
+//     }, 100);
+
+//     return () => clearInterval(interval);
+//   }, [isPaused]);
+
+//   useEffect(() => {
+//     const checkCollision = () => {
+//       const movingDiv = movingDivRef.current;
+//       if (movingDiv) {
+//         const carRect = movingDiv.getBoundingClientRect();
+//         answers.forEach((answer) => {
+//           const answerElement = document.getElementById(`answer-${answer.id}`);
+//           if (answerElement) {
+//             const answerRect = answerElement.getBoundingClientRect();
+
+//             const isColliding =
+//               carRect.left < answerRect.right &&
+//               carRect.right > answerRect.left &&
+//               carRect.top < answerRect.bottom &&
+//               carRect.bottom > answerRect.top;
+
+//             if (isColliding) {
+//               if (answer.text === currentQuestion?.answer) {
+//                 setScore(score + 5);
+//                 setCorrectAnswers(correctAnswers + 1);
+//                 setStatus('correct');
+//               } else {
+//                 setWrongAnswers(wrongAnswers + 1);
+//                 setStatus('wrong');
+//               }
+//               const nextQuestion =
+//                 questions[questions.indexOf(currentQuestion!) + 1];
+//               setCurrentQuestion(nextQuestion);
+//               setAnswers([]);
+
+//               if (!nextQuestion) {
+//                 setShowModal(true);
+//               }
+
+//               setTimeout(() => {
+//                 setStatus('idle');
+//               }, 500);
+//             }
+//           }
+//         });
+//       }
+//     };
+
+//     const interval = setInterval(checkCollision, 100);
+//     return () => clearInterval(interval);
+//   }, [
+//     answers,
+//     currentQuestion,
+//     move,
+//     questions,
+//     score,
+//     correctAnswers,
+//     wrongAnswers,
+//   ]);
+
+//   useEffect(() => {
+//     const handleKeyPress = (event: KeyboardEvent) => {
+//       switch (event.key) {
+//         case 'ArrowUp':
+//           setPosition('up');
+//           break;
+//         case 'ArrowDown':
+//           setPosition('down');
+//           break;
+//         default:
+//           break;
+//       }
+//     };
+
+//     window.addEventListener('keydown', handleKeyPress);
+
+//     return () => {
+//       window.removeEventListener('keydown', handleKeyPress);
+//     };
+//   }, []);
+
+//   useEffect(() => {
+//     if (position === 'up') {
+//       setMove(20);
+//     } else if (position === 'down') {
+//       setMove(200);
+//     }
+//   }, [position]);
+
+//   useSound('/sound/background-for-car.mp3');
+
+//   useEffect(() => {
+//     console.log('status: ', status);
+//   }, [status]);
+
+//   useEffect(() => {
+//     setIsPaused(false);
+//     setCurrentYear('Year 1');
+//     setCurrentLevel('Level 1');
+//   }, []);
+
+//   const calculateStars = (): number => {
+//     const totalQuestions = correctAnswers + wrongAnswers;
+//     const correctPercentage = (correctAnswers / totalQuestions) * 100;
+
+//     if (correctPercentage >= 90) {
+//       return 3;
+//     } else if (correctPercentage >= 70) {
+//       return 2;
+//     } else {
+//       return 1;
+//     }
+//   };
+
+//   return (
+//     <div
+//       style={{
+//         height: '100%',
+//         backgroundColor: '#444',
+//       }}
+//     >
+//       <div className={classes.gameWrapper}>
+//         <div className={classes.title}>
+//           <h1>Challenge</h1>
+//         </div>
+
+//         <div className={classes.gameCenter}>
+//           <div className={classes.gameCenterLeft}>
+//             <LeaderBoard />
+//           </div>
+//           <div className={classes.gameCenterMiddle}>
+//             {/* <div className={classes.carContainer}>
+//               <div ref={roadRef} className={classes.road}>
+//                 <div
+//                   ref={movingDivRef}
+//                   className={classes.car}
+//                   style={{ top: `${move}px` }}
+//                 >
+//                   <img
+//                     src='/assets/car/smoke.png'
+//                     className={classes.smokeImage}
+//                   />
+//                   <img src='/assets/car/red.png' className={classes.carImage} />
+//                 </div>
+//                 <div className={classes.lane}></div>
+//                 <div className={classes.centerLine}></div>
+//                 <div className={classes.lane}></div>
+//                 {answers.map((answer) => (
+//                   <div
+//                     key={answer.id}
+//                     id={`answer-${answer.id}`}
+//                     className={classes.answer}
+//                     style={{
+//                       top: `${answer.position}px`,
+//                       left: `${answer.left}px`,
+//                     }}
+//                   >
+//                     {answer.text}
+//                   </div>
+//                 ))}
+//               </div>
+//             </div> */}
+//             <div className={classes.question}>
+//               <h1>{currentQuestion ? currentQuestion.question : ''}</h1>
+//             </div>
+//           </div>
+//           <div className={classes.gameCenterRight}>
+//             <PlayerStat
+//               score={score}
+//               correctAnswers={correctAnswers}
+//               wrongAnswers={wrongAnswers}
+//             />
+//           </div>
+//         </div>
+//       </div>
+
+//       <Overlay opened={showModal} close={() => setShowModal(false)}>
+//         <GamePopupModal title='You Win'>
+//           <div
+//             style={{
+//               display: 'flex',
+//               flexDirection: 'column',
+//               justifyContent: 'center',
+//               alignItems: 'center',
+//               gap: 20,
+//             }}
+//           >
+//             <StarRating score={calculateStars()} size='large' />
+//             <h1 style={{ color: '#ffffff', fontSize: 30 }}>Congratulations!</h1>
+//             <div
+//               style={{
+//                 display: 'flex',
+//                 gap: 10,
+//               }}
+//             >
+//               <ButtonIcon>Replay</ButtonIcon>
+//               <ButtonIcon>Next</ButtonIcon>
+//               <ButtonIcon>Settings</ButtonIcon>
+//             </div>
+//           </div>
+//         </GamePopupModal>
+//       </Overlay>
+
+//       {status === 'correct' && (
+//         <div
+//           style={{
+//             width: '100%',
+//             height: '100%',
+//             position: 'absolute',
+//             backgroundColor: 'rgba(136, 255, 0, 0.2)',
+//             zIndex: 9,
+//             top: 0,
+//             left: 0,
+//             pointerEvents: 'none',
+//           }}
+//         />
+//       )}
+
+//       {status === 'wrong' && (
+//         <div
+//           style={{
+//             width: '100%',
+//             height: '100%',
+//             position: 'absolute',
+//             backgroundColor: 'rgba(255, 85, 0, 0.2)',
+//             zIndex: 9,
+//             top: 0,
+//             left: 0,
+//             pointerEvents: 'none',
+//           }}
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Car;
---
+++ src/components/Game/PicturePuzzle/Game copy.tsx
@@ -0,0 +1,325 @@
+import classes from './Game.module.css';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../../app/store';
+import Puzzle from './Puzzle';
+import QuestionsPage from './Questions';
+import { revealPiece, selectRandomPiece, addTime, decrementTimer, resetGame } from '../../../features/puzzleSlice';
+import questionsData from '../../../data/questions/questions.json'; // Import questions
+import confetti from 'canvas-confetti'; // Import canvas-confetti
+import { motion } from 'framer-motion';
+import profilePhoto from '../../../../public/assets/bear-profile-photo.png'
+import Progress from '../../Shared/Progress/Progress';
+import Overlay from '../../Shared/Overlay/Overlay';
+import GamePopupModal from '../../Modals/GamePopupModal/GamePopupModal';
+import GameScoreModal from '../../Modals/GameScoreModal/GameScoreModal';
+
+const imageUrls = [
+  '/assets/bear-profile-photo.png',
+  '/assets/avatar/african avatar.png',
+  '/assets/avatar/asian avatar.png',
+  '/assets/avatar/boy avatar.png',
+  '/assets/avatar/cute avatar.png',
+  '/assets/avatar/excited avatar.png',
+  '/assets/avatar/fashion boy.png',
+  '/assets/avatar/girl avatar.png',
+  '/assets/avatar/glass-girl avatar.png',
+  '/assets/avatar/teacher avatar.png',
+];
+
+const backgroundColors = [
+  'bg-blue-400',
+  'bg-green-400',
+  'bg-red-400',
+  'bg-yellow-400',
+];
+
+
+interface QuestionType {
+  id: number;
+  question: string;
+  answer: string;
+}
+
+const players = [
+  { first_name: 'Ziyech', last_name: 'Hakim', level: 5 },
+  { first_name: 'Mount', last_name: 'Mason', level: 4 },
+  { first_name: 'Mainoo', last_name: 'Kobbie', level: 3 },
+  { first_name: 'Garnacho', last_name: 'Alejandro', level: 2 },
+  { first_name: 'Ziyech', last_name: 'Hakim', level: 5 },
+  { first_name: 'Mount', last_name: 'Mason', level: 4 },
+  { first_name: 'Mainoo', last_name: 'Kobbie', level: 3 },
+  { first_name: 'Garnacho', last_name: 'Alejandro', level: 2 },
+];
+
+
+const variants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
+
+const twoColors: ProgressProps['strokeColor'] = {
+  '0%': '#108ee9',
+  '100%': '#87d068',
+};
+
+const Game: React.FC = () => {
+  const dispatch = useDispatch();
+  const state = useSelector((state: RootState) => state.puzzle);
+  const [feedbackPiece, setFeedbackPiece] = useState<number | null>(null);
+  const [showPopup, setShowPopup] = useState<boolean>(false);
+  const [borderColor, setBorderColor] = useState<string>('');
+  const [questions] = useState<QuestionType[]>(questionsData); // Use questions from JSON
+  const [score, setScore] = useState<number>(0);
+  const [gameEnded, setGameEnded] = useState<boolean>(false);
+  const [showModal, setShowModal] = useState(false);
+
+  // State to store random image URLs and background colors
+  const [playerImages, setPlayerImages] = useState<string[]>([]);
+  const [playerBackgroundColors, setPlayerBackgroundColors] = useState<string[]>([]);
+
+  useEffect(() => {
+    // Generate random images and background colors once on component mount
+    const images = players.map(() => imageUrls[Math.floor(Math.random() * imageUrls.length)]);
+    const colors = players.map(() => backgroundColors[Math.floor(Math.random() * backgroundColors.length)]);
+
+    setPlayerImages(images);
+    setPlayerBackgroundColors(colors);
+  }, [players]);
+
+
+  // Timer effect
+  useEffect(() => {
+    if (state.timeLeft > 0 && !gameEnded) {
+      const timer = setInterval(() => {
+        dispatch(decrementTimer());
+      }, 1000);
+      return () => clearInterval(timer);
+    } else if (!gameEnded) {
+      handleGameEnd();
+    }
+  }, [dispatch, state.timeLeft, gameEnded]);
+
+  const wonAudio = new Audio('./sound/point.wav');
+  const lostAudio = new Audio('./sound/negative.wav');
+
+  // Handle game end
+  const handleGameEnd = () => {
+    setGameEnded(true);
+    setShowModal(true);
+    confetti({
+      particleCount: 100,
+      spread: 70,
+      origin: { y: 0.6 },
+    });
+  };
+
+  // End game when timer reaches 0
+  useEffect(() => {
+    if (state.timeLeft <= 0) {
+      setShowModal(true)
+    }
+  }, [state.timeLeft]);
+
+  const handleAnswerSubmit = (answer: string) => {
+    if (state.timeLeft <= 0 || state.selectedPiece === null || gameEnded) return;
+
+    const question = questions[state.selectedPiece];
+    if (!question) return; // Check if question exists
+
+    if (answer.toLowerCase() === question.answer.toLowerCase()) {
+      dispatch(revealPiece());
+      setScore((prevScore) => prevScore + 1);
+
+      if (state.revealedPieces.length + 1 === questions.length) {
+        // All questions have been answered
+        handleGameEnd();
+        return;
+      }
+
+      dispatch(selectRandomPiece());
+      dispatch(addTime(3));
+      setFeedbackPiece(state.selectedPiece);
+      setBorderColor('border-green-500');
+      setShowPopup(true);
+      wonAudio.play();
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 },
+      });
+      setTimeout(() => setShowPopup(false), 1000);
+    } else {
+      setFeedbackPiece(state.selectedPiece);
+      setBorderColor('border-red-500');
+      lostAudio.play();
+    }
+
+    setTimeout(() => {
+      setFeedbackPiece(null);
+      setBorderColor('');
+    }, 1000);
+  };
+
+  // Restart the game
+  const restartGame = () => {
+    setShowModal(false); // Close modal before restarting game
+    dispatch(resetGame()); // Reset game state in Redux
+    setScore(0); // Reset score
+    setGameEnded(false); // Set game status
+    dispatch(selectRandomPiece()); // Ensure a question is selected
+  };
+
+  // Ensure `state.selectedPiece` is within bounds of `questions` array
+  const currentQuestion = state.selectedPiece !== null ? questions[state.selectedPiece] : null;
+
+  return (
+    // <main className="min-h-screen container mx-auto p-5 bg-white-to-black ">
+    <main className="min-h-screen container mx-auto p-5">
+      <div className={classes.title}>
+        <h1>Picture Puzzle</h1>
+      </div>
+      <div className='flex justify-center items-center space-x-3 mx-10'>
+        <div className='md:w-1/5'>
+          <div className='h-[38rem] bg-blue-400 rounded-xl p-2 backdrop-blur-sm bg-opacity-10 z-10 backdrop shadow-xl text-gray-800'>
+            <div className='leader-board'>
+              <p className='leader-board-title'>LeaderBoard</p>
+              <div className='leader-board-players'>
+                {players.map((item, index) => (
+                  <motion.div
+                    variants={variants}
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.95 }}
+                    className='leader-board-player'
+                    key={index.toString()}
+                  >
+                    <div className='mx-auto w-full'>
+                      <div className='flex items-center py-1 px-1'>
+                        <span className={`w-10 h-10 rounded-full object-cover ${playerBackgroundColors[index]} flex justify-center items-center`}>
+                          <img src={playerImages[index]} alt={`${item.first_name}'s profile`} className='h-9' />
+                        </span>
+                        <div className='flex items-center px-2'>
+                          <p className='text-lg mr-4 text-black'>{item.first_name}</p>
+                          <p className='text-sm text-black'>Lv{index + 1}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </motion.div>
+                ))}
+              </div>
+            </div>
+          </div>
+
+
+
+
+        </div>
+
+        <div className='w-3/5'>
+          <section className="flex-1 bg-blue-400 bg-opacity-10 backdrop-blur-sm rounded-2xl mb-2">
+            <Puzzle feedbackPiece={feedbackPiece} revealedPieces={state.revealedPieces} borderColor={borderColor} />
+          </section>
+
+
+          <section className="flex-1 bg-gradient-to-b  from-[#397eb7] to-blue-400 rounded-lg shadow-lg flex items-center justify-center">
+            {currentQuestion ? (
+              <QuestionsPage question={currentQuestion} onAnswerSubmit={handleAnswerSubmit} />
+            ) : (
+              <p>No question selected</p>
+            )}
+          </section>
+        </div>
+
+        <div className='w-1/5'>
+          <div className='h-[38rem] bg-blue-400 rounded-xl backdrop-blur-sm bg-opacity-20 z-10 backdrop shadow-xl text-gray-800'>
+            <div>
+              <div className="flex items-center p-2">
+                <span className="w-16 h-16 rounded-full object-cover mr-4 bg-blue-400 flex justify-center items-center">
+                  <img src={profilePhoto} alt={`${name}'s profile`} className='h-12' />
+
+                </span>
+                <div className="flex-1">
+                  <h2 className="text-2xl text-black font-semibold">Nathan</h2>
+                  <p className="text-gray-800 text-sm"><span className='mr-2'> &#x2022;</span>Class</p>
+                  <p className="text-gray-800 text-sm"> <span className='mr-2'> &#x2022;</span>School</p>
+                </div>
+              </div>
+              <p className='text-center text-black'>LEVEL</p>
+
+              <Progress />
+            </div>
+            <div>
+              <div className='flex items-center mt-3 bg-[#397eb7]'>
+                <p className='bg-black mr-2 text-center p-3.5 py-5 text-yellow-400'>TIME</p>
+                <div className='ml-6'>
+                  <p className='text-yellow-400 text-center text-6xl'>{state.timeLeft}</p>
+                  <p className='text-sm text-yellow-400 text-center '>Seconds Left</p>
+                </div>
+              </div>
+
+
+              <div className='flex justify-apart items-center py-1'>
+                <p className='w-1/3 mr-2 text-center py-3 text-black'>12</p>
+                <p className='text-lg text-black'>Correct answers</p>
+              </div>
+              <hr className="border-gray-500 border-t-2" />
+              <div className='flex justify-apart items-center py-1'>
+                <p className='w-1/3 mr-2 text-center py-3 text-black'>12</p>
+                <p className='text-lg text-black'>Incorrect answers</p>
+              </div>
+
+            </div>
+            <div className='bg-[#397eb7] pb-4'>
+              <h3 className='text-center text-white pt-2'>INSTRUCTIONS</h3>
+              <p className='text-center text-sm  mt-2'>1.) Click on the correct option to reveal a picture piece</p>
+              {/* <p className='text-center text-sm mt-2'>2.) Click on the correct option to reveal a picture piece</p> */}
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+
+
+      {showPopup && (
+        <div className="fixed top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blue-500 text-white p-4 rounded shadow-lg">
+          +3 seconds!
+        </div>
+      )}
+
+      <Overlay opened={showModal} >
+        <GameScoreModal title='Your Result'>
+          <div className='mt-2 py-12'>
+            <div className='h-24 w-24 bg-indigo-800 rounded-full mx-auto flex justify-center items-center'>
+              <div>
+                <p className='text-7xl text-center'> {score}</p>
+                <p className='text-xs text-gray-300 text-center'>of 30</p>
+              </div>
+            </div>
+            <div onClick={restartGame} className='flex mt-12 cursor-pointer justify-center items-center text-white bg-indigo-900 hover:bg-indigo-700 py-1 px-4 rounded '>
+              Replay
+            </div>
+          </div>
+        </GameScoreModal>
+      </Overlay>
+
+
+
+
+    </main>
+  );
+};
+
+export default Game;
