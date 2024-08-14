59ab43979771da129871ab473692679acc597173
joshua <joshuanwosu078@gmail.com>
2024-08-14 02:53:35 +0100

    update: login and update profile service

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,11 +1,16 @@
-import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  User,
+} from 'firebase/auth';
 import { auth } from '../configs/firebase';
 
 export const registerUserService = async (
   email: string,
   password: string,
   displayName: string
-) => {
+): Promise<User> => {
   const userCredential = await createUserWithEmailAndPassword(
     auth,
     email,
@@ -13,4 +18,22 @@ export const registerUserService = async (
   );
   await updateProfile(userCredential.user, { displayName });
   return userCredential.user;
+};
+
+export const loginUserService = async (
+  email: string,
+  password: string
+): Promise<User> => {
+  const userCredential = await signInWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+  return userCredential.user;
+};
+
+export const updateUserProfile = async (displayName: string): Promise<void> => {
+  if (auth.currentUser) {
+    await updateProfile(auth.currentUser, { displayName });
+  }
 };
