ccccc08b5b891a33e0ab1d07b43214d72ca78f69
joshua <joshuanwosu078@gmail.com>
2024-11-26 02:45:41 +0100

    update: some juice

--- src/components/Mission/Mission.tsx
+++ src/components/Mission/Mission.tsx
@@ -18,20 +18,33 @@ const Mission = ({ onPress }: MissionProps) => {
       exit={{ opacity: 0, y: '100%' }}
       transition={{ duration: 0.5 }} // Animation duration
     >
-      <div className={classes.board}>
-        <div className={classes.content}>
-          <h1>Mission</h1>
-          <div className={classes.body}>
-            <p className={classes.title}>Hey, firefighter!</p>
-            <p>Extinguishing a forest fire and rescuing animals.</p>
+      {false && (
+        <div className={classes.board}>
+          <div className={classes.content}>
+            <h1>Mission</h1>
+            <div className={classes.body}>
+              <p className={classes.title}>Hey, firefighter!</p>
+              <p>Extinguishing a forest fire and rescuing animals.</p>
+            </div>
+          </div>
+
+          <div className={classes.continueBtn} onClick={handleClick}>
+            <img src='assets/mission/button.png' />
+            <h4>Contine</h4>
           </div>
         </div>
+      )}
 
-        <div className={classes.continueBtn} onClick={handleClick}>
-          <img src='assets/mission/button.png' />
-          <h4>Contine</h4>
-        </div>
-      </div>
+      <img
+        src='assets/mission/doctor_mission/mission1_modal.png'
+        style={{
+          width: 500,
+          height: 500,
+          objectFit: 'contain',
+          objectPosition: 'center',
+        }}
+        onClick={handleClick}
+      />
     </motion.div>
   );
 };
