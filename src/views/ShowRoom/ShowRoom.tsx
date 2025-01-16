965c373727470d107002aebee3e3365c5ede5a5f
joshua <joshuanwosu078@gmail.com>
2025-01-16 20:12:27 +0100

    update: some juice

--- src/views/ShowRoom/ShowRoom.tsx
+++ src/views/ShowRoom/ShowRoom.tsx
@@ -2,7 +2,7 @@ import ShowRoom1 from './ShowRoom1';
 import ShowRoom2 from './ShowRoom2';
 
 export default function ShowRoom() {
-  const isTrue = true;
+  const isTrue = false;
 
   return <>{isTrue ? <ShowRoom1 /> : <ShowRoom2 />}</>;
 }
--- src/views/ShowRoom/ShowRoom2.tsx
+++ src/views/ShowRoom/ShowRoom2.tsx
@@ -1,5 +1,75 @@
-import classes from './ShowRoom1.module.css';
+import { useEffect, useState } from 'react';
+import classes from './Showroom2.module.css';
+import { getAvatarsByGender, IAvatar } from '../../data/showroom/images';
 
 export default function ShowRoom2() {
-  return <div className={classes.container}></div>;
+  const skinColors = ['black', 'white'];
+  const [characters, setCharacters] = useState<IAvatar[]>();
+
+  useEffect(() => {
+    setCharacters(getAvatarsByGender('male'));
+  }, []);
+
+  // Create a dummy array of 10 objects
+  const dummyArray = Array.from({ length: 10 }, (_, index) => ({
+    id: index + 1,
+    name: `Item ${index + 1}`,
+    value: Math.floor(Math.random() * 100), // Random value for variety
+  }));
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.flexContainer}>
+        <div className={classes.rightSection}>
+          <div className={classes.rightSectionInner}>
+            <div className={classes.characterThumbnail}>
+              {characters?.map((c, index) => (
+                <div
+                  key={index.toString()}
+                  className={classes.characterAvatar}
+                ></div>
+              ))}
+            </div>
+            <div className={classes.character}></div>
+            <div className={classes.characterSkin}>
+              {skinColors?.map((c, index) => (
+                <div
+                  key={index.toString()}
+                  className={classes.characterAvatar}
+                ></div>
+              ))}
+            </div>
+          </div>
+
+          <div className={classes.characterVehicle}></div>
+        </div>
+        <div className={classes.leftSection}>
+          <div className={classes.characterIventory}>
+            <h1 className={classes.characterIventoryTitle}>FireFighter</h1>
+            <div className={classes.characterIventoryProps}></div>
+          </div>
+
+          <div className={classes.characterIventory}>
+            <h1 className={classes.characterIventoryTitle}>Doctor</h1>
+            <div className={classes.characterIventoryProps}></div>
+          </div>
+
+          <div className={classes.characterIventory}>
+            <h1 className={classes.characterIventoryTitle}>Police</h1>
+            <div className={classes.characterIventoryProps}></div>
+          </div>
+
+          <div className={classes.characterIventory}>
+            <h1 className={classes.characterIventoryTitle}>Engineer</h1>
+            <div className={classes.characterIventoryProps}></div>
+          </div>
+
+          <div className={classes.characterIventory}>
+            <h1 className={classes.characterIventoryTitle}>Scientist</h1>
+            <div className={classes.characterIventoryProps}></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
