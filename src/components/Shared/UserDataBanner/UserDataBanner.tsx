5238fdafdf93cfdd9eab7eb0322011887bb3e400
joshua <joshuanwosu078@gmail.com>
2025-01-20 22:24:21 +0100

    update: some juice

--- src/components/Shared/UserDataBanner/UserDataBanner.tsx
+++ src/components/Shared/UserDataBanner/UserDataBanner.tsx
@@ -3,13 +3,17 @@ import classes from './UserDataBanner.module.css';
 export default function UserDataBanner() {
   return (
     <div className={classes.container}>
-      <img
-        src='/assets/elements/user_details_banner.png'
-        style={{
-          width: '542px',
-          height: '110px',
-        }}
-      />
+      <div className={classes.frame}>
+        <img
+          src='/assets/elements/user_details_banner.png'
+          className={classes.frameImage}
+        />
+        <div className={classes.frameFlex}>
+          <div className={classes.frameLeft}></div>
+          <div className={classes.frameMiddle}></div>
+          <div className={classes.frameRight}></div>
+        </div>
+      </div>
     </div>
   );
 }
