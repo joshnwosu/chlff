0eb47359ec4aa1cd3c212016b7f0dcae735470fd
joshua <joshuanwosu078@gmail.com>
2025-01-21 22:34:51 +0100

    update: some juice

--- src/components/Game/Fish/Fish.tsx
+++ src/components/Game/Fish/Fish.tsx
@@ -518,12 +518,11 @@ export default function Fish({ mode }: FishProps) {
             />
           </div>
 
-          {showGameOverModal && (
+          {!showGameOverModal && (
             <FishAssessmentGameOver
               score={correctAnswers}
               selected_year={selectedYear}
               total_questions={questions.length}
-              visible={showGameOverModal}
               strengthLevel={strengthLevel}
               handleReplayGame={handleReplayGame}
               showGameOverModal={showGameOverModal}
