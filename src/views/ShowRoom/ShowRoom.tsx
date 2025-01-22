0a45ef8f813c8e993b84754c735eda0e45128fcd
joshua <joshuanwosu078@gmail.com>
2025-01-22 23:41:01 +0100

    update: some juice

--- src/views/ShowRoom/ShowRoom.tsx
+++ src/views/ShowRoom/ShowRoom.tsx
@@ -1,8 +1,9 @@
 import ShowRoom1 from './ShowRoom1';
-import ShowRoom2 from './ShowRoom2';
+// import ShowRoom2 from './ShowRoom2';
+import ShowRoomEdit from './ShowRoomEdit';
 
 export default function ShowRoom() {
   const isTrue = false;
 
-  return <>{isTrue ? <ShowRoom1 /> : <ShowRoom2 />}</>;
+  return <>{isTrue ? <ShowRoom1 /> : <ShowRoomEdit />}</>;
 }
