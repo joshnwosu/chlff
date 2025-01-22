538e3fbe4d0f94414970c1cbdeb2b0453d09f90a
joshua <joshuanwosu078@gmail.com>
2025-01-22 22:44:47 +0100

    update: some juice

--- src/components/Modals/LeaderBoardInfoModal/LeaderBoardInfoModal.tsx
+++ src/components/Modals/LeaderBoardInfoModal/LeaderBoardInfoModal.tsx
@@ -1,6 +1,8 @@
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
 import { toggleShowLeadeBoardInfoModal } from '../../../features/control/controlSlice';
+import CustomButton from '../../Shared/CustomButton/CsutomButton';
 import Overlay from '../../Shared/Overlay/Overlay';
+import classes from './LeaderBoardInfoModal.module.css';
 
 export default function LeaderBoardInfoModal() {
   const dispatch = useAppDispatch();
@@ -12,7 +14,37 @@ export default function LeaderBoardInfoModal() {
 
   return (
     <Overlay opened={showLeaderBoardInfoModal} close={handleClose}>
-      <div>Hello</div>
+      <div className={classes.centeredView}>
+        <div className={classes.container}>
+          <div className={classes.left_panel}>
+            <div className={classes.profile}></div>
+
+            <div className={classes.profile_info}>
+              <p className={classes.profile_name}>Nathan Bravo</p>
+              <p className={classes.profile_year}>Year 1</p>
+            </div>
+
+            <div className={classes.game_info}>
+              <div className={classes.game_time}>
+                <p className={classes.game_label}>Time Played</p>
+                <p className={classes.game_value}>5 hours, 24 minutes</p>
+              </div>
+              <div className={classes.game_mission}>
+                <p className={classes.game_label}>Successful Mission</p>
+                <p className={classes.game_value}>4/5 played</p>
+              </div>
+            </div>
+
+            {/* <button></button> */}
+            <div className={classes.btn}>
+              <CustomButton color='red' onClick={handleClose}>
+                Close
+              </CustomButton>
+            </div>
+          </div>
+          <div className={classes.right_panel}></div>
+        </div>
+      </div>
     </Overlay>
   );
 }
