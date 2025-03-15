3eab942bae51e94620eb49ae57b619ee375c3fe9
joshua <joshuanwosu078@gmail.com>
2025-03-15 01:51:53 +0100

    bunch of updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     rollupOptions: {
       external: [], // No external modules
-      input: 'src/main.tsx', // Ensure entry point is correct
     },
   },
 });
